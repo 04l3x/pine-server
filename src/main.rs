7498409bf787f9e0087ec722eedcb8f3bf3470b6
0al3x <alex.vacan.bala@gmail.com>
2021-10-20 03:56:00 -0500

    basic configuration from cli args

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ mod models;
 mod server;
 mod utils;
 
+use server::Server;
+use utils::config::Config;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	utils::config::Config::run();
-	server::Server::start().await
+	match Config::run() {
+		_ => Server::start().await,
+	}
 }
--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,20 +1,52 @@
+use crate::error::Result;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Args {
+	#[structopt(short, long, default_value = "none")]
+	migrations: String,
+
+	#[structopt(short, long)]
+	data_example: bool,
+
+	#[structopt(short, long, default_value = ".env")]
+	env_file: PathBuf,
+}
 
 pub struct Config;
-//TODO: read options flags from command line
+
 impl Config {
-	pub fn run() {
-		env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+	pub fn run() -> Result<()> {
+		let args = Args::from_args();
+		Config::set_environment_variables(&args.env_file);
+		match env::var("ENVIRONMENT") {
+			Ok(var) => match var.as_str() {
+				"prod" | "production" => {
+					env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+				}
+				"dev" | "development" => {
+					env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+				}
+				"test" | "testing" => {
+					env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+				}
+				_ => {
+					env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+				}
+			},
+			Err(_) => {
+				env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+			}
+		}
 		env_logger::init();
-		//TODO: set rust log for different environments
-		//TODO: verify system dependencies when init ?
-		Config::set_environment_variables();
+		Ok(())
 	}
 
-	fn set_environment_variables() {
-		let file = fs::read_to_string(PathBuf::from(".env")).unwrap();
+	fn set_environment_variables(path: &PathBuf) {
+		let file = fs::read_to_string(path).unwrap();
 		for line in file.lines() {
 			let var: Vec<&str> = line.split('=').collect();
 			env::set_var(var[0], var[1]);
@@ -41,3 +73,39 @@ mod basics {
 		todo!();
 	}
 }
+
+/*struct Environment {
+	environment: String,
+	host: String,
+	port: String,
+	psql_host: String,
+	psql_port: String,
+	psql_user: String,
+	psql_password: String,
+	psql_default_schema: String,
+	psql_default_database: String,
+	git_root_dir: String,
+	git_storage_dir: String,
+	client_path: String,
+	secret_key: String,
+}
+
+impl Default for Environment {
+	fn default() -> Self {
+		Self {
+			environment: String::from("dev"),
+			host: String::from("localhost"),
+			port: String::from("9000"),
+			psql_host: String::from("localhost"),
+			psql_port: String::from("5432"),
+			psql_user: String::from("dev"),
+			psql_password: String::from("password123"),
+			psql_default_schema: String::from("git"),
+			psql_default_database: String::from("git_server_dev"),
+			git_root_dir: String::from("/git"),
+			git_storage_dir: String::from("/git/storage"),
+			client_path: String::from("../pine-client"),
+			secret_key: String::from("a_secure_secret_key"),
+		}
+	}
+}*/
